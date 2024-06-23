779615c1c0c890d9f14173809d1912d9cf3d40dc
Daniel Escobar <danielescobar.987@gmail.com>
2024-06-22 19:34:53 -0600

    LocalStorage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, Injector, computed, effect, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { Title } from '@angular/platform-browser';
 import { Task } from '../../models/task.model';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
@@ -13,18 +12,19 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal<Task[]>([
-    {
-      id: Date.now(),
-      title: 'Crear proyecto',
-      completed: false
-    },
-    {
-      id: Date.now(),
-      title: 'Crear componentes',
-      completed: false
-    },
-  ]);
+  tasks = signal<Task[]>([]);
+  filter =signal<'all' | 'pending' | 'completed'>('all');
+  taskByFilter = computed(() =>{
+    const filter = this.filter();
+    const tasks = this.tasks();
+    if(filter === 'pending'){
+      return tasks.filter(task => !task.completed)
+    }
+    if(filter === 'completed'){
+      return tasks.filter(task => task.completed)
+    }
+    return tasks;
+})
 
   newTaskCtrl = new FormControl('', {
     nonNullable: true,
@@ -32,11 +32,33 @@ export class HomeComponent {
       Validators.required,
     ]
   });
+  
+  injector = inject(Injector)
+
+  ngOnInit() {
+    const storage = localStorage.getItem('tasks');
+    if (storage){
+      const tasks = JSON.parse(storage)
+      this.tasks.set(tasks);
+    }
+    this.trackTasks();
+  }
+
+  trackTasks(){
+    effect(()=>{
+      const tasks = this.tasks();
+      console.log(tasks);
+      localStorage.setItem('tasks', JSON.stringify(tasks))
+    }, {injector: this.injector});
+  }
+
   changeHandler(){
     if(this.newTaskCtrl.valid){
-      const value = this.newTaskCtrl.value;
+      const value = this.newTaskCtrl.value.trim();
+      if (value !== ''){
       this.addTask(value);
       this.newTaskCtrl.setValue('');
+      }
     }
   }
   addTask(title: string){
@@ -61,6 +83,40 @@ export class HomeComponent {
         }
       return task;
     })
-  })
+  });
 }
+  updateTaskEditingMode(index: number){
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+          ...task,
+          editing: true
+          }
+        }
+      return {
+        ...task,
+          editing: false
+      }
+    })
+  });
+  }
+  updateTaskText(index: number, event:Event){
+    const input = event.target as HTMLInputElement
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+          ...task,
+          title: input.value,
+          editing:false
+          }
+        }
+      return task;
+    })
+  });
+  }
+  changeFilter(filter:'all' | 'pending' | 'completed'){
+    this.filter.set(filter);
+  }
 }
